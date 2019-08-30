83e46b946f6e3498e70894f9ad2f1f4297248515
Bernard Nandwa <bernard.nandwa@andela.com>
2019-08-30 21:07:28 +0300

    Create a profile model

---
+++ app/Models/Profile.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model{
+    protected $table = 'profiles';
+
+    protected $fillable = [
+        'first_name',
+        'middle_name',
+        'last_name'
+    ];
+}
